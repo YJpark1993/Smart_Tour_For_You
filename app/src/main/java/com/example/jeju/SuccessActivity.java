5f3756abce9510da40cbf0c7fc44f27c1a5be5fc
ehaannnn <ehaannnn@gmail.com>
2017-06-19 13:35:22 +0900

    Webview 수정중

--- app/src/main/java/com/example/jeju/SuccessActivity.java
+++ app/src/main/java/com/example/jeju/SuccessActivity.java
@@ -2,6 +2,7 @@ package com.example.jeju;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -9,9 +10,10 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
-import android.webkit.WebSettings;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TabHost;
 import android.widget.Toast;
 
 import org.mospi.moml.framework.pub.core.MOMLFragmentActivity;
@@ -23,33 +25,88 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.CookieStore;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
-import java.util.ArrayList;
 
 public class SuccessActivity extends MOMLFragmentActivity {
-    double latitude=35.1234, longitude=12.2345;
-    LocationManager locManager; // 위치 정보 프로바이더
-    LocationListener locationListener; // 위치 정보가 업데이트시 동작
-    Location location;
-    WebView webview;
+    private double latitude = 35.1234, longitude = 12.2345;
+    private LocationManager locManager; // 위치 정보 프로바이더
+    private LocationListener locationListener; // 위치 정보가 업데이트시 동작
+    private Location location;
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
-        loadApplication("/moml/applicationInfo_2.xml");
+        //loadApplication("/moml/applicationInfo_2.xml");
         //MOMLView momlView2 = (MOMLView) findViewById(R.id.momlView2);
         //setMomlView(momlView2);
 
+        final TabHost host = (TabHost) findViewById(R.id.tabHost);
+        host.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                if (tabId.equalsIgnoreCase("Tab Two")) {
+                    try {
+                        //doJSONParser();
+                    } catch (Exception e) {
+                        //Toast.makeText(MainActivity.this, "먼저 항목을 체크해주세요", Toast.LENGTH_LONG).show();
+                        host.setCurrentTab(0);
+                    }
+                } else if (tabId.equalsIgnoreCase("Tab One")) {
+                    //saveCheck.setProgress(0);
+                }
+            }
+        });
+        host.setup();
+
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+
+        spec.setContent(R.id.tab1);
+        //spec.setIndicator("", ResourcesCompat.getDrawable(getResources(), R.drawable.check, null));
+        spec.setIndicator("b");
+        host.addTab(spec);
+
+
+        //Tab 2
+        spec = host.newTabSpec("Tab Two");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("a");
+        //spec.setIndicator("", ResourcesCompat.getDrawable(getResources(), R.drawable.search, null));
+        host.addTab(spec);
+
+    /*    webView = (WebView) findViewById(R.id.webview);
+        //webview.setWebViewClinet(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                String urlString = webView.getUrl().toString();
+            }
+        });
+
+        webView.loadUrl("http://ec2-54-167-71-119.compute-1.amazonaws.com");
+*/
         locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(getApplicationContext(), "네트워크 설정을 해주세요", Toast.LENGTH_SHORT).show();
             return;
         }
+
         location = locManager.getLastKnownLocation(locManager.NETWORK_PROVIDER);
         locationListener = new LocationListener() {
             @Override
@@ -78,52 +135,62 @@ public class SuccessActivity extends MOMLFragmentActivity {
             longitude = location.getLongitude();
         }
 
-        locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                1000, 0, locationListener);
+        locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, locationListener);
         new PostHTTPServer().execute();
         onStop();
-
     }
 
     private class PostHTTPServer extends AsyncTask<Void, Void, String> {
-        String strUrl;
-        URL Url;
-        String strCookie;
-        String result;
-        BufferedWriter writer;
+        private String strUrl;
+        private URL Url;
+        private String strCookie;
+        private String result;
+        private BufferedWriter writer;
+
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
             strUrl = "http://ec2-54-167-71-119.compute-1.amazonaws.com/map.php"; //탐색하고 싶은 URL이다.
         }
+
         protected String doInBackground(Void... unused) {
             String content = executeClient();
             Log.d("xxx", "doInBackground function");
             return content;
         }
+
         // 모두 작업을 마치고 실행할 일 (메소드 등등)
         protected void onPostExecute(String result) {
             try {
-               webview = (WebView)findViewById(R.id.webView);
-                WebSettings webSettings = webview.getSettings();
-                webSettings.setJavaScriptEnabled(true);
+                webView = (WebView) findViewById(R.id.webview);
+                //webview.setWebViewClinet(new WebViewClient());
+                webView.getSettings().setJavaScriptEnabled(true);
 
-               webview.setWebViewClient(new WebViewClient() {
-                   @Override
-                   public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                       view.loadUrl(url);
-                       return true;
-                   }
-               });
-               webview.loadUrl(strUrl);
+                webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+                webView.setWebViewClient(new WebViewClient() {
+                    @Override
+                    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                        view.loadUrl(url);
+                        return true;
+                    }
+
+                    @Override
+                    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                        String urlString = webView.getUrl().toString();
+                    }
+                });
+
+                webView.loadUrl(strUrl);
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+
         // 실제 전송하는 부분
         public String executeClient() {
             Log.d("xxx", "executeClient function");
-            try{
+            try {
                 Url = new URL(strUrl); // URL화 한다.
                 HttpURLConnection conn = (HttpURLConnection) Url.openConnection(); // URL을 연결한 객체 생성.
                 conn.setRequestMethod("POST"); //
@@ -140,7 +207,7 @@ public class SuccessActivity extends MOMLFragmentActivity {
                 OutputStream outStream = conn.getOutputStream();
                 writer = new BufferedWriter(new OutputStreamWriter(outStream, "UTF-8"));
                 writer.write(
-                        "latitude="+latitude +"&longitude="+longitude); //요청 파라미터를 입력
+                        "latitude=" + latitude + "&longitude=" + longitude); //요청 파라미터를 입력
                 writer.flush();
                 writer.close();
                 outStream.close();
@@ -149,21 +216,21 @@ public class SuccessActivity extends MOMLFragmentActivity {
                 InputStream is = conn.getInputStream(); //input스트림 개방
 
                 StringBuilder builder = new StringBuilder(); //문자열을 담기 위한 객체
-                BufferedReader reader = new BufferedReader(new InputStreamReader(is,"UTF-8")); //문자열 셋 세팅
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8")); //문자열 셋 세팅
                 String line;
 
                 while ((line = reader.readLine()) != null) {
-                    builder.append(line+ "\n");
+                    builder.append(line + "\n");
                 }
 
                 result = builder.toString();
-            }catch(MalformedURLException | ProtocolException exception) {
+            } catch (MalformedURLException | ProtocolException exception) {
                 exception.printStackTrace();
-            }catch(IOException io){
+            } catch (IOException io) {
                 io.printStackTrace();
             }
             return result;
-
         }
+
     }
 }
